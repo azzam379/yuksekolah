3e1977ab99295f9f704ba284bb28842c3678a087
azzam379 <azzammadu37@gmail.com>
2026-01-21 22:41:10 +0700

    coba deploy

--- backend1/config/cors.php
+++ backend1/config/cors.php
@@ -4,6 +4,7 @@ return [
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
     'allowed_methods' => ['*'],
     'allowed_origins' => [
+        env('FRONTEND_URL', 'http://localhost:3000'),
         'http://localhost:3000',
         'http://127.0.0.1:3000',
     ],
